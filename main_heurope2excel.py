af40f328f7fb30233804d9652381d20bda957d6b
Carlos Beltran-Gonzalez <carlos.beltran@iit.it>
2022-02-28 20:18:24 +0100

    Filtering based on stage and temporal drop

--- main_heurope2excel.py
+++ main_heurope2excel.py
@@ -3,25 +3,43 @@ from pprint import pprint
 from datetime import datetime
 import pandas as pd
 
+# open the json file
+# download from:
+# https://ec.europa.eu/info/funding-tenders/opportunities/data/referenceData/grantsTenders.json
 with open('./grantsTenders.json','r',encoding='utf-8') as f:
     data = json.load(f) 
 
+# extract grants objects
 tenderobjs = data['fundingData']['GrantTenderObj']
-df = pd.DataFrame(columns=('CallIdentifier','CallTitle','Deadline'))
+
+# Create the pandas dataframe
+df = pd.DataFrame(columns=('CallIdentifier','CallTitle','FirstStage','Deadline'))
+
+# Iterate the grands object filling the dataframe with the data of interest
 i = 0
 for item in tenderobjs:
     try:
-        #deadline = datetime.fromtimestamp(item['deadlineDatesLong'][0]/1000).strftime('%d/%m/%Y %H:%M:%S')
-        deadline = datetime.fromtimestamp(item['deadlineDatesLong'][0]/1000).strftime('%d/%m/%Y')
-        #print(f"{item['callIdentifier']} {item['callTitle']} {deadline}")
-        df.loc[i] = [item['callIdentifier'],item['title'],deadline]
+        if len(item['deadlineDatesLong']) > 1: # there is two stages in the call
+            firstdeadline = datetime.fromtimestamp(item['deadlineDatesLong'][0]/1000).strftime('%d/%m/%Y')
+            deadline = datetime.fromtimestamp(item['deadlineDatesLong'][1]/1000).strftime('%d/%m/%Y')
+        else: # just one stage
+            firstdeadline = 0
+            deadline = datetime.fromtimestamp(item['deadlineDatesLong'][0]/1000).strftime('%d/%m/%Y')
+        df.loc[i] = [item['callIdentifier'],item['title'],firstdeadline,deadline]
     except:
         pass
     i = i + 1
 
+# Drop dupplicates if any
 df = df.drop_duplicates()
+
+# sort the entries cronologicaly
 df['Deadline'] = pd.to_datetime(df['Deadline'],format='%d/%m/%Y')
 df = df.sort_values(by='Deadline')
+
+# remove the entries older than a give date (e.g. today)
+df.drop(df.loc[df['Deadline'] < '2022-02-28 01:00:00'].index, inplace=True)
 print(df.head(3))
 
+# export to excel
 df.to_excel('eucalls.xlsx',index=False, header = True)
